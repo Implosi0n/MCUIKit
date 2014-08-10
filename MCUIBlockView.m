172e37341e15875d9114e5addb7620a35bf61e07
Silas Schwarz <redwing.climb@gmail.com>
2014-08-10 10:48:21 -0600

    added slab rendering

--- MCUIBlockView.m
+++ MCUIBlockView.m
@@ -23,15 +23,44 @@
 		CGFloat theta = M_PI / 6.0 - 0.185; /* AAAAAAAAHHH Please, somebody stop me */
 		
 		CATransform3D rotateAndTilt = CATransform3DMakeRotation(-M_PI/4, 0, 1, 0);
-		rotateAndTilt = CATransform3DConcat(rotateAndTilt, CATransform3DMakeRotation(theta, 1, 0, 0));
 		
 		MCUIImageType type = [UIImage mcui_typeOfImageWithID:itemID damage:damage];
 		NSArray *images = [UIImage mcui_imagesWithID:itemID damage:damage];
-		if (!images || !images.count) {
-			return self;
+		UIImage *image1 = [images[0] mcui_resize:CGSizeMake(faceWidth, faceWidth)];
+		UIImage *image2 = [images[0] mcui_resize:CGSizeMake(faceWidth, faceWidth)];
+		UIImage *image3 = [images[0] mcui_resize:CGSizeMake(faceWidth, faceWidth)];
+		
+		switch (type) {
+			case kMCUIImageBlock:
+				break;
+			case kMCUIImageMultiBlock:
+				image2 = [images[1] mcui_resize:CGSizeMake(faceWidth, faceWidth)];
+				image3 = [images[2] mcui_resize:CGSizeMake(faceWidth, faceWidth)];
+				break;
+			case kMCUIImageSlab:
+			{
+				UIImage *snapshotImage = images[1];
+				snapshotImage = [snapshotImage mcui_subImageWithFrame:CGRectMake(0, 0, snapshotImage.size.width, snapshotImage.size.height / 2.0)];
+				snapshotImage = [snapshotImage mcui_resize:CGSizeMake(faceWidth, faceWidth / 2.0)];
+				
+				UIGraphicsBeginImageContext(CGSizeMake(faceWidth, faceWidth));
+				CGContextRef c = UIGraphicsGetCurrentContext();
+				CGContextSetInterpolationQuality(c, kCGInterpolationNone);
+				[snapshotImage drawInRect:CGRectMake(0, 0, faceWidth, faceWidth / 2.0)];
+				snapshotImage = UIGraphicsGetImageFromCurrentImageContext();
+				UIGraphicsEndImageContext();
+				image2 = snapshotImage;
+				image3 = snapshotImage;
+				rotateAndTilt = CATransform3DTranslate(rotateAndTilt, 0, faceWidth / 2.0, 0);
+			}
+				break;
+			default:
+				break;
 		}
 		
-		topImageView = [[UIImageView alloc] initWithImage:[images[0] mcui_resize:CGSizeMake(faceWidth, faceWidth)]];
+		rotateAndTilt = CATransform3DConcat(rotateAndTilt, CATransform3DMakeRotation(theta, 1, 0, 0));
+		
+		topImageView = [[UIImageView alloc] initWithImage:[image1 mcui_resize:CGSizeMake(faceWidth, faceWidth)]];
 		topImageView.frame = (CGRect){ .origin = CGPointZero, .size = topImageView.frame.size };
 		topImageView.layer.anchorPoint = CGPointMake(0, 0);
 		[self addSubview:topImageView];
@@ -39,7 +68,7 @@
 		topImageView.layer.transform = CATransform3DMakeRotation(M_PI/2, 1, 0, 0);
 		topImageView.layer.transform = CATransform3DConcat(topImageView.layer.transform, rotateAndTilt);
 		
-		leftImageView = [[UIImageView alloc] initWithImage:[images[type == 0 ? 0 : 1] mcui_resize:CGSizeMake(faceWidth, faceWidth)]];
+		leftImageView = [[UIImageView alloc] initWithImage:[image2 mcui_resize:CGSizeMake(faceWidth, faceWidth)]];
 		leftImageView.frame = (CGRect){ .origin = CGPointZero, .size = leftImageView.frame.size };
 		leftImageView.layer.anchorPoint = CGPointMake(0, 0);
 		[self addSubview:leftImageView];
@@ -53,7 +82,7 @@
 		
 		[leftImageView addSubview:leftDarkener];
 		
-		rightImageView = [[UIImageView alloc] initWithImage:[images[type == 0 ? 0 : 2] mcui_resize:CGSizeMake(faceWidth, faceWidth)]];
+		rightImageView = [[UIImageView alloc] initWithImage:[image3 mcui_resize:CGSizeMake(faceWidth, faceWidth)]];
 		rightImageView.frame = (CGRect){ .origin = CGPointZero, .size = rightImageView.frame.size };
 		rightImageView.layer.anchorPoint = CGPointMake(0, 0);
 		[self addSubview:rightImageView];
@@ -72,6 +101,21 @@
 		topImageView.layer.transform = CATransform3DConcat(topImageView.layer.transform, relocate);
 		leftImageView.layer.transform = CATransform3DConcat(leftImageView.layer.transform, relocate);
 		rightImageView.layer.transform = CATransform3DConcat(rightImageView.layer.transform, relocate);
+		
+		switch (type) {
+			case kMCUIImageBlock:
+				break;
+			case kMCUIImageMultiBlock:
+				break;
+			case kMCUIImageSlab:
+			{
+				leftDarkener.frame = CGRectMake(leftDarkener.frame.origin.x, leftDarkener.frame.origin.y, leftDarkener.frame.size.width, leftDarkener.frame.size.height / 2.0);
+				rightDarkener.frame = CGRectMake(rightDarkener.frame.origin.x, rightDarkener.frame.origin.y, rightDarkener.frame.size.width, rightDarkener.frame.size.height / 2.0);
+			}
+				break;
+			default:
+				break;
+		}
 	}
 	return self;
 }
